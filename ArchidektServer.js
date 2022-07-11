f608b730dbd262c55b994b701fd8f0ba3d5bac18
Blink Tenor <blinktenor@gmail.com>
2022-07-10 20:39:51 -0500

    Getting ready for vercel

--- ArchidektServer.js
+++ ArchidektServer.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 80;
 const archidekt = require('archidekt');
 require("regenerator-runtime/runtime");
 const cors = require('cors')
 
-app.use(cors())
-
-function censor(censor) {
-  var i = 0;
-  
-  return function(key, value) {
-    if(i !== 0 && typeof(censor) === 'object' && typeof(value) == 'object' && censor == value) 
-      return '[Circular]'; 
-    
-    if(i >= 29) // seems to be a harded maximum of 30 serialized objects?
-      return '[Unknown]';
-    
-    ++i; // so we know we aren't using the original object anymore
-    
-    return value;  
+var allowlist = ['https://dollarydo.vercel.app']
+var corsOptionsDelegate = function (req, callback) {
+  var corsOptions;
+  console.log(req.header('Origin'));
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true } // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false } // disable CORS for this request
   }
+  callback(null, corsOptions) // callback expects two parameters: error and options
 }
 
 app.get('/folder/:folderId', (req, res) => {
@@ -38,8 +32,7 @@ app.get('/folder/:folderId', (req, res) => {
 app.get('/decks/:deckId', async (req, res) => {
   const deckId = req.params.deckId;
   const response = await archidekt.default.fetchDeckById(deckId);
-  console.log(response.data);
-  res.write(JSON.stringify(response.data));
+  res.send(JSON.stringify(response.data)); 
 })
 
 app.listen(port, () => {
